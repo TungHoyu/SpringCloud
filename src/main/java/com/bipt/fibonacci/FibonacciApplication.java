c74d61c9573c273547422207f6079af5c1cd1de7
huizi <623138924@qq.com>
2022-06-29 15:58:39 +0800

    测试提交

--- src/main/java/com/bipt/fibonacci/FibonacciApplication.java
+++ src/main/java/com/bipt/fibonacci/FibonacciApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FibonacciApplication {
 
+    //测试提交
     public static void main(String[] args) {
         SpringApplication.run(FibonacciApplication.class, args);
     }
